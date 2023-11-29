3a72377547050bfc1571c1df33bd67a225c11498
AlexisMarez <alexismeraz04@gmail.com>
2023-11-29 14:53:56 -0700

    actually added users except bios

--- All_Project_Code/src/init_data/create.sql
+++ All_Project_Code/src/init_data/create.sql
@@ -1,4 +1,28 @@
 CREATE TABLE IF NOT EXISTS users (
-    username VARCHAR(50) PRIMARY KEY,
-    password CHAR(60) NOT NULL
+    user_id INTEGER PRIMARY KEY,
+    username VARCHAR NOT NULL,
+    password VARCHAR NOT NULL,
+    bio VARCHAR NOT NULL,
+    location VARCHAR NOT NULL,
+    age INTEGER NOT NULL,
+    photo_id INTEGER NOT NULL,
+    preferences_id INTEGER NOT NULL
+
 );
+
+CREATE TABLE IF NOT EXISTS preferences{
+    preferences_id INTEGER PRIMARY KEY,
+    age_range INTEGER NOT NULL,
+    sex VARCHAR NOT NULL,
+    pets INTEGER NOT NULL,
+    budget INTEGER NOT NULL,
+};
+
+CREATE TABLE IF NOT EXISTS photos{
+    photo_id INTEGER PRIMARY KEY,
+    photo_1 NOT NULL,
+    photo_2 NOT NULL,
+    photo_3 NOT NULL,
+    photo_4 NOT NULL,
+    photo_5 NOT NULL
+};
