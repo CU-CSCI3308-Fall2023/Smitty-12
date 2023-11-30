f4fdd8cc2ef8b9875c3cb0defb4ce01888fc6ba8
keviny769 <keya8248@colorado.edu>
2023-11-29 21:09:09 -0700

    added matches table, added logo on navbar

--- All_Project_Code/src/init_data/create.sql
+++ All_Project_Code/src/init_data/create.sql
@@ -3,5 +3,12 @@ CREATE TABLE IF NOT EXISTS users (
     password CHAR(60) NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS matches (
+    match_id SERIAL PRIMARY KEY,
+    user_id_from INT,
+    user_id_to INT,
+    FOREIGN KEY (user_id_from) REFERENCES users (user_id),
+    FOREIGN KEY (user_id_to) REFERENCES users (user_id)
+);
 
 insert into users (username, password) values ('JohnDoe', '123');
