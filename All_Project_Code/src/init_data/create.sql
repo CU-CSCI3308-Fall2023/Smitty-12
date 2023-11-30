7955f0b83473adeca13d5ac41512bcf02f681787
AlexisMarez <alexismeraz04@gmail.com>
2023-11-29 18:52:18 -0700

    hopefully woking sql files

--- All_Project_Code/src/init_data/create.sql
+++ All_Project_Code/src/init_data/create.sql
@@ -1,7 +1,30 @@
-CREATE TABLE IF NOT EXISTS users (
-    username VARCHAR(50) PRIMARY KEY,
-    password CHAR(60) NOT NULL
+CREATE TABLE IF NOT EXISTS preferences(
+   preferences_id INTEGER PRIMARY KEY,
+   age_range INTEGER NOT NULL,
+   sex VARCHAR NOT NULL,
+   pets INTEGER NOT NULL,
+   budget INTEGER NOT NULL
 );
 
 
-insert into users (username, password) values ('JohnDoe', '123');
+CREATE TABLE IF NOT EXISTS photos(
+   photo_id INTEGER PRIMARY KEY,
+   photo_1 VARCHAR NOT NULL,
+   photo_2 VARCHAR NOT NULL,
+   photo_3 VARCHAR NOT NULL,
+   photo_4 VARCHAR NOT NULL,
+   photo_5 VARCHAR NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS users (
+   user_id INTEGER PRIMARY KEY,
+   username VARCHAR NOT NULL,
+   password VARCHAR NOT NULL,
+   bio VARCHAR NOT NULL,
+   location VARCHAR NOT NULL,
+   age INTEGER NOT NULL,
+   photo_id INTEGER REFERENCES photos(photo_id),
+   preferences_id INTEGER REFERENCES preferences(preferences_id)
+);
+
+
--- All_Project_Code/src/init_data/insert.sql
+++ All_Project_Code/src/init_data/insert.sql
@@ -1,1 +1,5 @@
-INSERT INTO users (username, password) VALUES ('JohnDoe', '123');
+INSERT INTO photos (photo_id, photo_1, photo_2, photo_3, photo_4, photo_5) VALUES (0, 'photo1_path', 'photo2_path', 'photo3_path', 'photo4_path', 'photo5_path');
+
+INSERT INTO preferences (preferences_id, age_range, sex, pets, budget) VALUES (0, 18, 'Male', 1, 1000);
+
+INSERT INTO users (user_id, username, password, bio, location, age, photo_id, preferences_id) VALUES (0,'JohnDoe', '123', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'Texas', 20, 0, 0);
